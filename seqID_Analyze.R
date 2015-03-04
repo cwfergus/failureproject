1a702a8bb979e88f99aa8d304d31d875d783d2fa
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-03-03 18:48:32 -0800

    Added modlist, and a testing region as I am on to something with summarise_each

--- seqID_Analyze.R
+++ seqID_Analyze.R
@@ -21,9 +21,22 @@ seqID_fail_rate <-
         arrange(desc(failure_rate)) %>%
         filter(failure_rate != 1)
         
+modlist <- 
+        clean_failure %>%
+        group_by(sequence_ID)%>%
+        select(sequence_ID, Five_Prime_mod, Three_Prime_mod)
+
+
 
 seqID_10percent <- quantile(seqID_fail_rate$failure_rate, probs=0.9)
 
 seqID_top10percent <- filter(seqID_fail_rate, failure_rate>= seqID_10percent)
 
 class(seqID_top10percent) <- "data.frame"
+
+#testing region
+
+seqID_failure_counts <-
+        clean_failure %>%
+        group_by(sequence_ID, Five_Prime_mod, Three_Prime_mod) %>%
+        summarise_each(funs(n()))
