5359f9d53f07c4e7a374ec39f51a23100e9c206d
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-03-05 13:07:54 -0800

    Commented my entire failure list code

--- failurelist.R
+++ failurelist.R
@@ -1,3 +1,6 @@
+#Failure List function
+
+#The below portion checks for exsistance of packages, and either downloads or loads them
 if (require(dplyr)==FALSE){
         install.packages(dplyr)
         library(dplyr)
@@ -16,59 +19,72 @@ if (require(stringr)==FALSE) {
 } else {
         library(stringr)
 }
+
+#Sourcing the unqiue functions that are used in this script
 source('failurelistfunctions.R')
 
+#Asks the user for the name of the raw data file, and appends the name with its extension
 filename <- readline("What is the data file called?... ")
 filename <- paste(filename, ".tab", sep="")
 
+#Asks the user for their desired output name, and appends the name with the xlsx extension
 outputname <- readline("What is the output file called?...  ")
 outputname <- paste(outputname, ".xlsx", sep="")
 
-rawdata <- read.table(filename, 
-                      sep="\t", 
-                      stringsAsFactors=FALSE, 
-                      na.strings = c("", " "),
-                      quote="",
-                      comment.char="")
+#reads in the raw data file
+rawdata <- read.table(filename, #user specified name
+                      sep="\t", #says that variables are seperated by TABS
+                      stringsAsFactors=FALSE, #prevents characters being turned into factors
+                      na.strings = c("", " "), #causes empty variables to be turned into NA's
+                      quote="", #prevents Quoting
+                      comment.char="") #prevents reading in # as a comment, not a variable
 
+#appends colnames to the data frame
 colnames(rawdata) <- c("Failure_Reason", 
                        "Five_Prime_mod", 
                        "Three_Prime_mod",
                        "sequence_ID")
+# converts the data frame to the special tbl_df class, necessary for use with DPLYR functions
 raw_tbl_df <- tbl_df(rawdata)
-
+#removes any observations that contain an NA in the Failure_Reason Variable.
 not_passed <- filter(raw_tbl_df, !is.na(Failure_Reason))
-
+#Cleans up the data, removing extra blank space and changing all notes to lower case.
 not_passed <- clean_up()
-
+#Removes not failed sequences, that contain notes, using the not_failed function
+#see failurelistfunctions.R for details
 only_failed <- not_failed()
-
+#Aggregates the failure reasons, to enable correct grouping, using the failure_aggregation
+#function. See failurelistfunctions.R for details
 clean_failure_reassign_msokay <- failure_aggregation()
-
+#Removes all Reassign notes from the data
 clean_failure_msokay <- reason_remover(clean_failure_reassign_msokay, "Reassigned")
-
+#removes all MS Okay notes from the data
 clean_failure <- reason_remover(clean_failure_msokay, "Ms Okay")
-
+#Groups, counts, and orders the data. Causes removal of all varialbes except Failure Reason
+#and the new number_of_failures column
 Failurelist_by_Reason <-
-        clean_failure %>%
-        group_by(Failure_Reason) %>%
-        summarize(number_of_failures = n()) %>%
-        arrange(desc(number_of_failures), Failure_Reason)
-
+        clean_failure %>% #runs the following functions on the clean_failure data frame
+        group_by(Failure_Reason) %>% # groups the data frame by failure_reason
+        summarize(number_of_failures = n()) %>% #counts the number of times a failure_reason
+                                                # occurs, writes new variable w/ data
+        arrange(desc(number_of_failures), Failure_Reason) # arranges the data
+#Generates the summary excel sheet. see summary.R for details
 source('summary.R')
-
+#converts the data back to a normal data frame, from its previous tbl_df class
 class(Failurelist_by_Reason) <- "data.frame"
+#writes out the data to an excel sheet
+write.xlsx(Failurelist_by_Reason,#the data
+           file=outputname, #the users specified outputname with the .xlsx extension
+           sheetName="Top Failure Reasons",#the sheet name
+           row.names=FALSE, #It won't try to add row names
+           append=TRUE) # It will add this sheet to an exsisting file if necessary.
 
-write.xlsx(Failurelist_by_Reason,
-           file=outputname, 
-           sheetName="Top Failure Reasons",
-           row.names=FALSE,
-           append=TRUE)
-
+#Generates the seqID sheets, see the seqID_Analyze.R script for details
 source('seqID_Analyze.R')
-
+#Generates the mod analysis sheets, see the mod_analyze.R script for details.
 source('mod_Analyze.R')
 
+#Removes everything from R memory.
 #rm(list=ls())
 
 
